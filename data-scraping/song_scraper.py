decc23f29d9924acb2540d8fbc95596566c722b6
Daniel B <daniel.m.bassett@outlook.com>
2023-01-07 17:20:29 -0600

    finished song scraper

--- data-scraping/song_scraper.py
+++ data-scraping/song_scraper.py
@@ -1,17 +1,34 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 import time
 import itertools
 
-response = requests.get('https://www.billboard.com/charts/year-end/2013/hot-dance-electronic-songs/')
-soup = BeautifulSoup(response.text, "html.parser")
-chart_results = soup.find(class_='chart-results-list')
-raw_titles = chart_results.find_all(class_='c-title')
-ranks_and_artists = chart_results.find_all(class_='c-label')
+years = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+genre = 'hip-hop-rb-songs'
 
-titles = [raw_titles[i].text.strip() for i in range(len(raw_titles))]
-ranks = [int(ranks_and_artists[i].text.strip()) for i in range(0, len(ranks_and_artists), 2)]
-artists = [ranks_and_artists[i].text.strip() for i in range(1, len(ranks_and_artists), 2)]
+lyrics_list = []
 
-for (rank, artist, titles) in zip(ranks, artists, titles):
-    print(rank, artist, titles)
+for year in years:
+    time.sleep(3)
+    response = requests.get(f'https://www.billboard.com/charts/year-end/{year}/hot-r-and-and-b-hip-hop-songs/')
+    soup = BeautifulSoup(response.text, "html.parser")
+    chart_results = soup.find(class_='chart-results-list')
+    raw_titles = chart_results.find_all(class_='c-title')
+    ranks_and_artists = chart_results.find_all(class_='c-label')
+
+    titles = [raw_titles[i].text.strip() for i in range(len(raw_titles))]
+    ranks = [int(ranks_and_artists[i].text.strip()) for i in range(0, len(ranks_and_artists), 2)]
+    artists = [ranks_and_artists[i].text.strip() for i in range(1, len(ranks_and_artists), 2)]
+
+    for (rank, artist, title) in zip(ranks, artists, titles):
+        print(year)
+        lyrics_list.append({
+            'title': title,
+            'artist': artist,
+            'rank': rank,
+            'genre': genre
+        })
+    
+df = pd.DataFrame(lyrics_list)
+df.to_csv('data\hip-hop-rb-songs.csv')
