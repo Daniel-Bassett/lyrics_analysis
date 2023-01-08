95f2a7a4b4656f7e9ec6d8fcebec162583381407
Daniel B <daniel.m.bassett@outlook.com>
2023-01-07 20:23:54 -0600

    added lyrics extraction using lyricsgenius

--- data-scraping/lyrics_scraper.py
+++ data-scraping/lyrics_scraper.py
@@ -3,3 +3,27 @@ import pandas as pd
 import requests
 import time
 import itertools
+from lyricsgenius import Genius
+from config import *
+
+genius = Genius(TOKEN, remove_section_headers=True)
+
+
+
+christian_songs_df = pd.read_csv('data\christian.csv')
+print(christian_songs_df)
+
+lyrics_list = []
+
+
+
+for row, col in christian_songs_df.iterrows():
+    song = genius.search_song(col['title'], col['artist'])
+    lyrics = song.lyrics.split()
+    for word in lyrics:
+        lyrics_list.append({
+            'word': word,
+            'artist': col['artist'],
+            'title': col['title'],
+
+        })
